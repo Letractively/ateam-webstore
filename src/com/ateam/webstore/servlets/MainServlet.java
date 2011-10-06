b19e320ac6908a927375d272219c34fffb0275e6
b.j.drew <b.j.drew@gmail.com>
2011-10-06 18:06:47 +0000

    Initial UI work.

--- src/com/ateam/webstore/servlets/MainServlet.java
+++ src/com/ateam/webstore/servlets/MainServlet.java
@@ -1,14 +1,22 @@
 package com.ateam.webstore.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ateam.webstore.ui.Constants;
+import com.ateam.webstore.ui.ContentView;
+import com.ateam.webstore.ui.View;
 
-public class MainServlet extends HttpServlet {
+
+public class MainServlet extends HttpServlet implements Constants {
 
 	/**
 	 * 
@@ -19,9 +27,39 @@ public class MainServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
+		View v = getMainView();
+		
+		if (req.getParameterMap().containsKey("login")) {
+			List<ContentView> contentViews = new ArrayList<ContentView>();
+			contentViews.add(getLoginView());
+			v.setContentViews(contentViews );
+		}
+		
+		req.setAttribute(REQUEST_ATTRIBUTE_VIEW, v);
+		
 		getServletConfig().getServletContext().getRequestDispatcher(
-				"main.jsp").forward(req, resp);
+				"/main.jsp").forward(req, resp);
 
+	}
+	
+	private View getMainView() {
+		View v = new View();
+		v.setTitle("A+Team WebStore");
+		v.setHeaderTitle("A+Team WebStore");
+		v.setHeaderSubText("pitty the foo");
+		Map<String, String> navLinks = new HashMap<String,String>();
+		navLinks.put("Login", "main?login");
+		v.setNavLinks(navLinks );
+		return v;
+
+	}
+	
+	private ContentView getLoginView () {
+		ContentView cv = new ContentView();
+		cv.setContentHeader("Login");
+		cv.setContentText("Please Login");
+		cv.setJspf("/login.jspf");
+		return cv;
 	}
 
 }
---
+++ src/com/ateam/webstore/ui/View.java
@@ -0,0 +1,50 @@
+package com.ateam.webstore.ui;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class View {
+	String title;
+	String headerTitle;
+	String headerSubText;
+	String footerText;
+	Map<String,String> navLinks;
+	List<ContentView> contentViews;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getHeaderTitle() {
+		return headerTitle;
+	}
+	public void setHeaderTitle(String headerTitle) {
+		this.headerTitle = headerTitle;
+	}
+	public String getHeaderSubText() {
+		return headerSubText;
+	}
+	public void setHeaderSubText(String headerSubText) {
+		this.headerSubText = headerSubText;
+	}
+	public String getFooterText() {
+		return footerText;
+	}
+	public void setFooterText(String footerText) {
+		this.footerText = footerText;
+	}
+	public Map<String, String> getNavLinks() {
+		return navLinks;
+	}
+	public void setNavLinks(Map<String, String> navLinks) {
+		this.navLinks = navLinks;
+	}
+	public List<ContentView> getContentViews() {
+		return contentViews;
+	}
+	public void setContentViews(List<ContentView> contentViews) {
+		this.contentViews = contentViews;
+	}
+}
