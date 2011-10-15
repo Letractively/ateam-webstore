e5b75abb6534a91b8fae69f8be866d28b2c18cbf
htavarez@gmail.com <htavarez@gmail.com@bee9de0c-8f4a-524e-ae4d-7e2a056df690>
2011-10-15 23:08:53 +0000

    clean up

--- trunk/src/ateamweb.sql
+++ trunk/src/ateamweb.sql
@@ -1,26 +1,61 @@
 --
 -- product
 --
+drop table PRODUCT;
 
-DROP TABLE PRODUCT;
+CREATE TABLE PRODUCT (
+	productID	INTEGER		NOT NULL AUTO_INCREMENT	,
+	productName	VARCHAR(150)	NOT NULL		,
+	SKU		CHAR(15)	NOT NULL		,
+	price		DECIMAL(10,2)	NOT NULL		,
+	percentDiscount	DECIMAL(6,4)	NOT NULL,
+	description	VARCHAR(1000)	NOT NULL		,
+	imagePath	CHAR(50)				,
+	firstOffered	DATE					,
+	saleInd		CHAR(1)					,
+	lastUpdated	TIMESTAMP				,
+		PRIMARY KEY (productID))
+		AUTO_INCREMENT=100001
+		ENGINE=INNODB
+;
 
-CREATE TABLE PRODUCT  (
-    product_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY , 
-    name VARCHAR(100) NOT NULL ,
-    price DOUBLE NOT NULL ,
-    description VARCHAR(512) NOT NULL 
-)
 
-insert into PRODUCT (product_id, name, price, description)
-values(1, "iMac", 5000.0, "iMac computer");
+insert into PRODUCT (SKU, productName, price, percentDiscount, description)
+values("AB", "iMac", 5000.00, 0, "iMac computer");
 
-insert into PRODUCT (product_id, name, price, description)
-values(2, "iPod", 500.0, "iPod music player");
+insert into PRODUCT (SKU, productName, price, percentDiscount, description)
+values("CD", "iPod", 500.00, 0, "iPod music player");
 
-insert into PRODUCT (product_id, name, price, description)
-values(3, "iPhone", 700.0, "iPhone smart phone");
+insert into PRODUCT (SKU, productName, price, percentDiscount, description)
+values("EF", "iPhone", 700.00, 0, "iPhone smart phone");
 
-insert into PRODUCT (product_id, name, price, description)
-values(4, "iPad", 1000.0, "iPad tablet");
+insert into PRODUCT (SKU, productName, price, percentDiscount, description)
+values("ZZ", "iPad", 1000.00, 0, "iPad tablet");
+
 
 select * from PRODUCT;
+delete from PRODUCT where productId=100001
+
+
+
+
+    <class name="Person" identity-type="application" detachable="true" >   
+      <field name="id" value-strategy="identity" primary-key="true"/>         
+      <field name="login"/>
+      <field name="password"/>
+      <field name="securityQuesID"/>
+      <field name="securityAnswer"/>
+    </class> 
+    
+    <class name="SecurityQuery" identity-type="application" detachable="true" >   
+      <field name="id" value-strategy="identity" primary-key="true"/>         
+      <field name="question"/>
+    </class>     
+    
+    <class name="Customer" identity-type="application" detachable="true" >   
+      <field name="id" value-strategy="identity" primary-key="true"/>         
+      <field name="login"/>
+      <field name="password"/>
+      <field name="securityQuesID"/>
+      <field name="securityAnswer"/>
+    </class> 
