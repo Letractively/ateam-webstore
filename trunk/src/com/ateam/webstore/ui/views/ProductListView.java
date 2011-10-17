c2112e6e08f26e3fdde669d95e9956eff4de1d90
vageeshtanda@gmail.com <vageeshtanda@gmail.com@bee9de0c-8f4a-524e-ae4d-7e2a056df690>
2011-10-17 17:37:19 +0000

    created view

---
+++ trunk/src/com/ateam/webstore/ui/views/ProductListView.java
@@ -0,0 +1,15 @@
+package com.ateam.webstore.ui.views;
+
+import com.ateam.webstore.ui.models.Product;
+
+public class ProductListView extends View {
+	Product product;
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
