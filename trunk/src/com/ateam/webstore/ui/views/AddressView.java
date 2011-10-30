4eda7db59b457a7c110af2f69da47761ebce95d7
vageeshtanda@gmail.com <vageeshtanda@gmail.com@bee9de0c-8f4a-524e-ae4d-7e2a056df690>
2011-10-30 22:14:55 +0000

    Code update

---
+++ trunk/src/com/ateam/webstore/ui/views/AddressView.java
@@ -0,0 +1,17 @@
+package com.ateam.webstore.ui.views;
+
+import com.ateam.webstore.model.Address;
+
+public class AddressView extends View {
+	Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+}
---
+++ trunk/src/com/ateam/webstore/ui/views/CreditCardView.java
@@ -0,0 +1,15 @@
+package com.ateam.webstore.ui.views;
+
+import com.ateam.webstore.model.CreditCard;
+
+public class CreditCardView extends View {
+	CreditCard creditcard;
+	
+	public CreditCard getCreditcard() {
+		return creditcard;
+	}
+	public void setCreditcard(CreditCard creditcard) {
+		this.creditcard = creditcard;
+	}
+
+}
