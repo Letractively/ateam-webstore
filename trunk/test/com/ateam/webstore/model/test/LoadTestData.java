c1c4b9c875602594ed92951ad493acdfe71b34c5
htavarez@gmail.com <htavarez@gmail.com@bee9de0c-8f4a-524e-ae4d-7e2a056df690>
2011-11-13 23:53:10 +0000

    Unit Testing

---
+++ trunk/test/com/ateam/webstore/model/test/LoadTestData.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.ateam.webstore.model.test;
+
+import com.ateam.webstore.model.Customer;
+import com.ateam.webstore.model.Person;
+import com.ateam.webstore.service.impl.CustomerService;
+import com.ateam.webstore.service.impl.PersonService;
+
+/**
+ * @author Hendrix Tavarez
+ *
+ */
+public class LoadTestData {
+	
+	
+	
+	public static void main(String[] args) {
+//		
+//		Brand brand = new Brand("Sony");
+//		new BrandService().store(brand);
+		
+//		ProductService prodService = new ProductService();
+//		Collection<Product> products = prodService.getAll();		
+//		for (Product product : products) {			
+//			System.out.println(product.getProductName() + " " + product.getSubCategory().getSubcategoryName());			
+//		}
+		
+		
+//		CustomerService custService = new CustomerService();
+//		Collection<Customer> customers = custService.getAll();
+//		for (Customer customer : customers) {
+//			System.out.println(customer.getPerson().getSecurityQuestion().getId());
+//		}
+		
+//		System.out.println((new SecurityQuestionService().getById(new Long(22))).getQuestion());
+		
+		PersonService ps = new PersonService();
+		Person person = ps.getByLoginAndPassword("valerie@pou.com", "password");
+		
+		System.out.println(person.getCustomer().getFirstName());
+		CustomerService cs = new CustomerService();
+//		Customer customer = cs.getByPersonId(person.getId());
+//		System.out.println(customer.getFirstName());
+		
+		
+	}
+
+}
