5ae78f27e99aa0bde1d43f8cde9e2e03122858b2
b.j.drew <b.j.drew@gmail.com>
2011-11-20 20:40:06 +0000

    EmployeeHandler stub

---
+++ src/com/ateam/webstore/handlers/EmployeeHandler.java
@@ -0,0 +1,62 @@
+package com.ateam.webstore.handlers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.ateam.webstore.model.Employee;
+import com.ateam.webstore.ui.forms.FormSubmission;
+import com.ateam.webstore.ui.views.ContentView;
+import com.ateam.webstore.ui.views.View;
+
+public class EmployeeHandler extends Handler {
+
+	public EmployeeHandler(HttpServletRequest req) {
+		super(req);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Employee getEmployee() {
+		// TODO Auto-generated constructor stub
+		return null;
+	}
+	public View getEmployeeListView() {
+		// TODO Auto-generated constructor stub
+		return null;
+	}
+	public View getEmployeeDetailsView() {
+		// TODO Auto-generated constructor stub
+		return null;
+	}
+	public FormSubmission processLoginRequest() {
+		// TODO Auto-generated constructor stub
+		return null;
+	}
+	public FormSubmission processAddEmployeeRequest() {
+		// TODO Auto-generated constructor stub
+		return null;
+	}
+	/**
+	 * Get the login view
+	 * @param loginMessage
+	 * @return
+	 */
+	public View getLoginView(String loginMessage) {
+		
+		View l = getMainView();
+		
+		l.setShowLogonForm(false);
+		
+		ContentView cv = new ContentView(JSP_LOGIN, "Login");
+		
+		if (loginMessage != null) {
+			l.setMessage(loginMessage);
+		}
+		
+		l.addContentView(cv);
+		l.setServletPath("admin");
+		
+		return l;
+
+	}
+
+
+}
